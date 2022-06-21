3b44dbd5c69829399840c4a5a2bde773fc76891c
afiffifa <mafifny@gmail.com>
2022-06-21 03:03:44 -0400

    First Commit

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,19 @@
+//
+//import app.Calculator;
+//import org.testng.Assert;
+//import org.testng.annotations.Test;
+//import org.testng.asserts.SoftAssert;
+//
+//public class CalculatorTest {
+//    @Test()
+//    public void addTest(){
+//        int a = 2;
+//        int b = 2;
+//        int expectedValue = 4;
+//        int actualResult = Calculator.add(a,b);
+////        Assert.assertEquals(expectedValue,actualResult);
+//        SoftAssert softAssert = new SoftAssert();
+//        softAssert.assertEquals(expectedValue,actualResult);
+//    }
+//
+//}
