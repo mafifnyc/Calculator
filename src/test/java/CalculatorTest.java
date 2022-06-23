a359d9d5cc0c37914bc578f64f5f0cc02338cf3b
afiffifa <mafifny@gmail.com>
2022-06-22 20:38:57 -0400

    Class Commit

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -11,29 +11,14 @@ public void addTest(){
         String b = "2";
         int expectedValue = 4;
         int actualResult = Calculator.add(a, b);
-
-        if(expectedValue == actualResult){
-        System.out.println("Test Passed");
-        }
-        else if (expectedValue !=actualResult){
-        System.out.println("Test Failed");
-        }
-
-        System.out.println("Oh Great Calculator App is working well...");
+        Assert.assertEquals(expectedValue, actualResult);
         }
 @Test()
 public void deductTest(){
-        // Reassigning the value to the actual variable  after calling deduct()
         String a = "2";
         String b = "2";
-        int expectedValue = -1;
-        int actualResult  =  Calculator.deduct();
-
-        if(expectedValue == actualResult){
-        System.out.println("Test Passed");
-        }
-        else if (expectedValue !=actualResult){
-        System.out.println("Test Failed");
-        }
+        int expectedValue = 0;
+        int actualResult  =  Calculator.deduct(a,b);
+        Assert.assertEquals(expectedValue, actualResult);
         }
 }
